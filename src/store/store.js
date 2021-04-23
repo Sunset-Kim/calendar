f1c7ebec78613f01d4492122f9e8acbe644d8a21
Sunset-Kim <wood2002@naver.com>
2021-04-23 22:12:04 +0900

    Implement list remove and sort list
    목록제거, 목록정렬 기능을 구현함

--- src/store/store.js
+++ src/store/store.js
@@ -23,16 +23,27 @@ export const store = new Vuex.Store({
       dateId: '',
     },
     getters: {
+      // 시간순서대로 정리된 list
+      sortList(state) {
+        return state.taskList.sort((a,b) => {
+          return a.time - b.time;
+        })
+      },
+      // 현재 tasklist의 등록된 날짜값을 중복없이 가지고 옴
       attDate(state){
         let date = state.taskList.map(element => element.taskDate);
-
         return new Set(date);
       }
     },
     mutations: {
+      // 현재날짜를 받아옴
       loadDate(state, payload) {
         state.dateId = payload.id;
       },
+      blurDate(state) {
+        state.dateId = '';
+      },
+      // task를 등록함
       addNewTask(state, taskItem) {
         //전송(저장)할 데이터 오브젝트를 만든다.
         let obj = {
@@ -52,13 +63,23 @@ export const store = new Vuex.Store({
         }
        
       },
-
+      // 완료된 리스트를 다시 로컬스토리지와 tasklist에 업데이트함
       toggleComplete(state,payload) {
         const result = state.taskList.find(element => {
           return element.taskId === payload.item.taskId;
         });
         result.completed = !result.completed;
+        localStorage.setItem(result.taskId,JSON.stringify(result));
+      },
+      // 아이템을 지운다.
+      removeTask(state,payload) {
+        const index = state.taskList.indexOf(payload.item)
+        if(index < 0) {
+          return
+        } else {
+          state.taskList.splice(index,1);
+          localStorage.removeItem(payload.item.taskId);
+        }
       }
-
     }
   })
